38fb047249a7bd8db1c305dca544ff037cc4b749
alexioannides <alex.ioanndies@yahoo.co.uk>
2020-11-12 17:21:30 +0000

    Complete serving stage.

--- stage-2-deploy-scoring-service/serve_model.py
+++ stage-2-deploy-scoring-service/serve_model.py
@@ -1,37 +1,77 @@
 """
-This module defines what will happen in stage-2-deploy-scoring-service.
+This module defines what will happen in 'stage-2-deploy-scoring-service':
 
-curl http://0.0.0.0:5000/iris/v1/predict \
+- download ML model and load into memory;
+- define ML scoring REST API endpoints; and,
+- start service.
+
+The scoring service can be tested from the command line using:
+
+curl http://0.0.0.0:5000/iris/v1/score \
     --request POST \
     --header "Content-Type: application/json" \
-    --data '{"X": [1, 2, 3, 4]}'
+    --data '{"sepal_length": 5.1, "sepal_width": 3.5, "petal_length": 1.4, "petal_width": 0.2}'
 """
 from urllib.request import urlopen
+from typing import Dict
 
-import joblib
 import numpy as np
 from flask import Flask, jsonify, make_response, request, Response
+from joblib import load
+from sklearn.base import BaseEstimator
+
+MODEL_URL = 'http://bodywork-example-ml-project.s3.eu-west-2.amazonaws.com/iris_tree_classifier.joblib'
+CLASS_TO_SPECIES_MAP = {0: 'setosa', 1: 'versicolor', 2: 'virginica'}
 
 app = Flask(__name__)
 
 
-@app.route('/iris/v1/predict', methods=['GET', 'POST'])
-def predict() -> Response:
-    X = np.array([request.json['X']])
-    predicted_class = (
-        model
-        .predict(X)
-        .tolist()
-        [0]
+@app.route('/iris/v1/score', methods=['POST'])
+def score() -> Response:
+    """Iris species classification API endpoint"""
+    request_data = request.json
+    X = make_features_from_request_data(request_data)
+    model_output = model_predictions(X)
+    response_data = jsonify({**model_output, 'model_info': str(model)})
+    return make_response(response_data)
+
+
+def get_model(url: str) -> BaseEstimator:
+    """Get model from cloud object storage."""
+    model_file = urlopen(url)
+    return load(model_file)
+
+
+def make_features_from_request_data(
+    request_data: Dict[str, float]
+) -> np.ndarray:
+    """Create feature array from JSON data parsed as dictionay."""
+    X = np.array(
+        [request_data['sepal_length'],
+         request_data['sepal_width'],
+         request_data['petal_length'],
+         request_data['petal_width']],
+        ndmin=2
     )
-    response_data = {
-        'prediction': predicted_class,
-        'model': str(model)
+    return X
+
+
+def model_predictions(features: np.ndarray) -> Dict[str, str]:
+    """Return model scores for a single instance of feature data."""
+    class_prediction = int(model.predict(features)[0])
+    class_probabilities = model.predict_proba(features)[0]
+    species_prediction = CLASS_TO_SPECIES_MAP[class_prediction]
+    species_probabilities = [
+        f'{k}={v}'
+        for k, v in zip(CLASS_TO_SPECIES_MAP.values(), class_probabilities)
+    ]
+    results = {
+        'species_prediction': species_prediction,
+        'probabilities': '|'.join(species_probabilities)
     }
-    return make_response(jsonify(response_data))
+    return results
 
 
 if __name__ == '__main__':
-    model_url = 'http://bodywork-example-ml-project.s3.eu-west-2.amazonaws.com/iris_tree_classifier.joblib'
-    model = joblib.load(urlopen(model_url))
+    model = get_model(MODEL_URL)
     app.run(host='0.0.0.0', port=5000)
